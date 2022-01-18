7e6ccd7073bb4506ff068a4d7b2822c25387a0ff
ailyn-aggarwal-1008 <88395699+ailyn-aggarwal-1008@users.noreply.github.com>
2022-01-18 14:29:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function colourchange(){
+    document.getElementById("heading").style.color="blue";
+  }
+  function pcolourchange(){
+    document.getElementById("para").style.color="#34E3D4";
+  }
